0ff664cc9a6401dbc0c3055bcfe3286fad99193e
uday kiran <udaykirandarla2002@gmail.com>
2024-08-16 23:28:04 +0530

    update1.1

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const jsonServer = require('json-server');
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(jsonServer.defaults());
 
-const router = jsonServer.router('db.json'); // Your JSON file
-app.use('/api', router);
+// Correct path to db.json
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+app.use('/app', router);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
